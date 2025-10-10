02ddbf2c91d628ccececdb29d4d709fa57d5b0e3
soytak <ssoitak1@gmail.com>
2025-10-10 17:40:40 -0400

    Made the pause menu accessible in many places Fix pauseMenu cursor bug Put the cursor and pauseMenu on a CanvasLayer

--- Game/pause/pauseMenu.gd
+++ Game/pause/pauseMenu.gd
@@ -1,9 +1,10 @@
-extends Control
+extends CanvasLayer
 
-@onready var unpause_button = $Panel/VBoxContainer/unpause
-@onready var exit_button = $Panel/VBoxContainer/exit
+@onready var unpause_button = $Control/Panel/VBoxContainer/unpause
+@onready var exit_button = $Control/Panel/VBoxContainer/exit
 @onready var dim_rect = $bg
-
+@onready var control = $Control
+@export var hidingCursor: bool = false
 
 var mainMenu = preload("res://Main menu/Menu.tscn")
 
@@ -17,24 +18,24 @@ func _ready() -> void:
 func show_menu() -> void:
 	show()
 	get_tree().paused = true
-	var screen_size = get_viewport_rect().size
-	var screen_width = get_viewport_rect().size.x
-	position = Vector2(-size.x, screen_size.y / 2 - size.y / 2)
+	var screen_size = control.get_viewport_rect().size
+	var screen_width = control.get_viewport_rect().size.x
+	control.position = Vector2(-control.size.x, screen_size.y / 2 - control.size.y / 2)
 	var tween = create_tween()
-	tween.tween_property(self, "position:x", 0, 0.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween.tween_property(control, "position:x", 0, 0.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 	#tween.parallel().tween_property(dim_rect, "color:a", 0.5, 0.5)
 	tween.finished.connect(func():
 		cursorManager.enableCursors()
 	)
 
 func hide_menu() -> void:
-	var screen_width = get_viewport_rect().size.x
+	var screen_width = control.get_viewport_rect().size.x
 	var tween = create_tween()
-	tween.tween_property(self, "position:x", -screen_width, 0.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
+	tween.tween_property(control, "position:x", -screen_width, 0.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
 	#tween.parallel().tween_property(dim_rect, "color:a", 0.0, 0.5)
 	tween.finished.connect(func ():
 		hide()
-		cursorManager.disableCursors()
+		if hidingCursor: cursorManager.disableCursors()
 		get_tree().paused = false
 	)
 
@@ -47,13 +48,13 @@ func _input(event: InputEvent) -> void:
 
 func _process(delta: float) -> void:
 	if visible:
+		
 		hue_shift += delta
 		unpause_button.modulate = Color.from_hsv(fmod(hue_shift, 1.0), 1.0, 1.0)
 
-func _on_exit_button_pressed() -> void:
-	get_tree().paused = false
-	get_tree().change_scene_to_packed(mainMenu)
-
-
 func _on_unpause_pressed() -> void:
 	hide_menu()
+
+func _on_exit_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_packed(mainMenu)
--- cursor_manager.gd
+++ cursor_manager.gd
@@ -1,4 +1,4 @@
-extends Control
+extends CanvasLayer
 
 var cursora = preload("res://cursor.tscn")
 var MinAFKTime: float = 5
@@ -6,17 +6,16 @@ var MinAFKTimeEnable: bool = false
 
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
-	z_index = 20
 	for i in range(4):
 		var newCursor = cursora.instantiate()
 		newCursor.enable()
 		newCursor.player = i+1
-		add_child(newCursor)
+		$Control.add_child(newCursor)
 
 func disableCursors():
-	for child in get_children():
+	for child in $Control.get_children():
 		child.disable()
 
 func enableCursors():
-	for child in get_children():
+	for child in $Control.get_children():
 		child.enable()
